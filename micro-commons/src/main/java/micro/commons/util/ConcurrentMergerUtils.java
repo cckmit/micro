cd6275faf3950f89ecd8a9005627ccc71b1e21d4
geweixinerr <geweixinerr@vip.163.com>
2022-03-01 21:34:19 +0800

    Fix: 归并计算优化

--- micro-commons/src/main/java/micro/commons/util/ConcurrentMergerUtils.java
+++ micro-commons/src/main/java/micro/commons/util/ConcurrentMergerUtils.java
@@ -1,15 +1,17 @@
 package micro.commons.util;
 
+import static org.apache.commons.collections4.CollectionUtils.isEmpty;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.Callable;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Function;
 import java.util.stream.IntStream;
 
-import org.apache.commons.collections.CollectionUtils;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.util.concurrent.ListenableFuture;
 
@@ -41,53 +43,134 @@ public final class ConcurrentMergerUtils {
 	 * 归并计算
 	 * 
 	 * @author gewx
-	 * @param execute   执行器
-	 * @param result    待计算数据.只支持List集合
-	 * @param taskDepth 任务深度(即一个任务分片分配多少数据)
+	 * @param execute 执行器
+	 * @param result  待计算数据.只支持List集合
+	 * @param depth   任务深度(即一个任务分片分配多少数据)
 	 * @return 归并结果
 	 **/
-	public static <T, R> List<R> calculate(Function<List<T>, R> execute, List<T> result, int taskDepth) {
-		if (CollectionUtils.isEmpty(result)) {
+	public static <T, R> List<R> calculate(Function<List<T>, R> execute, List<T> result, int depth) {
+		if (isEmpty(result)) {
 			return Collections.emptyList();
 		}
 
-		int allDepth = result.size();
-		int allTask = allDepth / taskDepth;
-		if (allDepth % taskDepth != 0) {
-			allTask = allTask + 1;
+		int taskCount = result.size() / depth;
+		if (result.size() % depth != 0) {
+			taskCount = taskCount + 1;
 		}
 
-		AtomicInteger count = new AtomicInteger(allTask);
-		AtomicBoolean state = new AtomicBoolean(true);
+		AtomicInteger downCount = new AtomicInteger(taskCount);
+		AtomicBoolean mark = new AtomicBoolean(true);
 		StringBuilder ex = new StringBuilder(512);
-		List<R> mergerList = new ArrayList<>(allTask * 2);
+		List<R> mergerList = new ArrayList<>(taskCount * 2);
 
-		// Fix: 此处可以新增账号,譬如从MDC中获取
-		IntStream.range(0, allTask).forEach(val -> {
+		IntStream.range(0, taskCount).forEach(val -> {
 			ListenableFuture<R> future = (ListenableFuture<R>) POOLTASKEXECUTOR.submitListenable(() -> {
-				int next = (taskDepth + (taskDepth * val));
-				List<T> subList = result.subList(taskDepth * val, next > result.size() ? result.size() : next);
+				if (!mark.get()) {
+					return null;
+				}
+				int next = (depth + (depth * val));
+				List<T> subList = result.subList(depth * val, next > result.size() ? result.size() : next);
 				return execute.apply(subList);
 			});
 
 			future.addCallback(r -> {
-				count.decrementAndGet();
 				mergerList.add(r);
+				downCount.decrementAndGet();
 			}, thx -> {
 				ex.append(thx.getMessage());
-				state.set(false);
+				mark.set(false);
 			});
 		});
 
 		while (true) {
-			if (count.intValue() == 0) {
+			if (downCount.intValue() == 0) {
 				break;
 			}
-			if (!state.get()) {
+			if (!mark.get()) {
 				throw new ConcurrentMergerException("归并计算异常, ex: " + ex.toString());
 			}
 		}
 		return mergerList;
+	}
+
+	/**
+	 * 归并计算
+	 * 
+	 * @author gewx
+	 * @param task 任务集合
+	 * @return 归并结果
+	 **/
+	@SuppressWarnings("unchecked")
+	public static <R> List<R> calculate(Callable<R>... task) {
+		AtomicInteger downCount = new AtomicInteger(task.length);
+		AtomicBoolean mark = new AtomicBoolean(true);
+		StringBuilder ex = new StringBuilder(512);
+		List<R> mergerList = new ArrayList<>(task.length * 2);
+
+		IntStream.range(0, task.length).forEach(index -> {
+			ListenableFuture<R> future = (ListenableFuture<R>) POOLTASKEXECUTOR.submitListenable(() -> {
+				if (!mark.get()) {
+					return null;
+				}
+				return task[index].call();
+			});
+
+			future.addCallback(r -> {
+				mergerList.add(r);
+				downCount.decrementAndGet();
+			}, thx -> {
+				ex.append(thx.getMessage());
+				mark.set(false);
+			});
+		});
+
+		while (true) {
+			if (downCount.intValue() == 0) {
+				break;
+			}
+			if (!mark.get()) {
+				throw new ConcurrentMergerException("归并计算异常, ex: " + ex.toString());
+			}
+		}
+		return mergerList;
+	}
+
+	/**
+	 * 并发执行
+	 * 
+	 * @author gewx
+	 * @param task 任务集合
+	 * @return 归并结果
+	 **/
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void calculate(Runnable... task) {
+		AtomicInteger downCount = new AtomicInteger(task.length);
+		AtomicBoolean mark = new AtomicBoolean(true);
+		StringBuilder ex = new StringBuilder(512);
+
+		IntStream.range(0, task.length).forEach(index -> {
+			ListenableFuture future = (ListenableFuture) POOLTASKEXECUTOR.submitListenable(() -> {
+				if (mark.get()) {
+					task[index].run();
+				}
+			});
+
+			future.addCallback(r -> {
+				downCount.decrementAndGet();
+			}, thx -> {
+				ex.append(thx.getMessage());
+				mark.set(false);
+			});
+		});
+
+		while (true) {
+			if (downCount.intValue() == 0) {
+				break;
+			}
+			if (!mark.get()) {
+				throw new ConcurrentMergerException("并发执行异常, ex: " + ex.toString());
+			}
+		}
 	}
 
 	/**
@@ -96,7 +179,7 @@ public final class ConcurrentMergerUtils {
 	 * @author gewx
 	 * @param result 归并结果集
 	 * @param func   结果集处理函数
-	 * @return 归并结果集
+	 * @return 归并结果
 	 **/
 	public static <T, R> List<R> getResult(List<T> result, Function<T, R> func) {
 		List<R> list = new ArrayList<>(64);
